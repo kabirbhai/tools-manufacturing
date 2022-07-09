ed1ca33086e1a1ef7fb13d971c70e3b3da54665b
kabir <makkobir79@gmail.com>
2022-07-09 23:06:55 +0600

    navbar created link

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRoute } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRoute>
+  <BrowserRouter>
     <React.StrictMode>
       <App />
     </React.StrictMode>
-  </BrowserRoute>
+  </BrowserRouter>
 );
--- src/pages/shared/Navbar.js
+++ src/pages/shared/Navbar.js
@@ -1,16 +1,29 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <header>
-      <nav className="flex py-4 shadow-md">
-        <div className="logo mr-6 uppercase">tools-manufacturing</div>
+      <nav className="flex py-4 shadow-md items-center">
+        <div className="logo mr-6 uppercase">
+          <Link className="text-2xl" to="/">
+            tools-MF
+          </Link>
+        </div>
         <div className="nav-items">
           <ul className="flex space-x-4">
-            <li>Home</li>
-            <li>About</li>
-            <li>Blog</li>
-            <li>Contact</li>
+            <li>
+              <Link to="/home"> Home</Link>
+            </li>
+            <li>
+              <Link to="/about"> About</Link>
+            </li>
+            <li>
+              <Link to="/blog"> Blog</Link>
+            </li>
+            <li>
+              <Link to="/contact"> Contact</Link>
+            </li>
           </ul>
         </div>
       </nav>
